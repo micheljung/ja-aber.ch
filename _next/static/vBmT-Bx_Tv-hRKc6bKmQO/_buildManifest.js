511c16c57d1b3b99f389b7568a626f6abc41e0ec
micheljung <micheljung@users.noreply.github.com>
2021-11-29 21:12:38 +0000

    deploy: f6afbc32180281f58a2c127a2199b8961dd1a901

---
+++ _next/static/vBmT-Bx_Tv-hRKc6bKmQO/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-c8036dad2ee88fab5d82.js"],"/_error":["static/chunks/pages/_error-d36babdecdd567a38448.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
